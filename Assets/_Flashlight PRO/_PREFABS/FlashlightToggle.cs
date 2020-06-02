b64f036cb4380547f4eac26bca3a7c38c87b61ab
Max Kim <maxjukim@gmail.com>
2020-06-01 19:44:43 -0700

    Cleaned up FL script and made it sync position

--- Assets/_Flashlight PRO/_PREFABS/FlashlightToggle.cs
+++ Assets/_Flashlight PRO/_PREFABS/FlashlightToggle.cs
@@ -7,13 +7,10 @@ using VRC.Udon;
 public class FlashlightToggle : UdonSharpBehaviour
 {
 
-    public GameObject FlashLight;
     public GameObject FLLight;
-    public bool on;
 
     void Start()
     {
-        on = false;
     }
 
     public override void OnPickupUseDown()
